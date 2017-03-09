bdc2a5fd5ee76acedb1024ad7710343d8212454a
Fernando <backovertime@gmail.com>
2017-03-10 00:20:57 +0100

    variables corregido

--- src/tema1/Variables_1.java
+++ src/tema1/Variables_1.java
@@ -5,7 +5,7 @@ public class Variables_1 {
 	public static void main(String[] args) {
 		
 		double numero= 214.5;
-		boolean luz= false;
+		boolean luz= true;
 		System.out.println(numero);
 		System.out.println(luz);
 
